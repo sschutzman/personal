c808050010543f236aaa4afb2f27c4df9a928ffb
sschutzman <$t735815>
2018-07-11 20:38:08 -0500

    added java to project

---
+++ src/main/java/com/personal/schutzman/SchutzmanApplication.java
@@ -0,0 +1,12 @@
+package com.personal.schutzman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchutzmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchutzmanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/personal/schutzman/SchutzmanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.personal.schutzman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchutzmanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
